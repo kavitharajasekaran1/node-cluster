86793be4d24a5666059a142690997a00f25fc35e
kavitharajasekaran1 <kavithasek@yahoo.co.in>
2023-08-07 12:27:06 -0400

    app.js changes

--- app.js
+++ app.js
@@ -10,24 +10,24 @@ app.get('/', (req,res)=>{
 
     }
     res.send(`...ok ${process.pid}`)
-    // cluster.worker.kill()
+    cluster.worker.kill()
 })
 
-// if(cluster.isMaster) {
+if(cluster.isMaster) {
 
-//     for(i=0;i< numCpu;i++) {
-//         cluster.fork() // uses the child process.
-//     }
-//     cluster.on('exit', (worker, code, signal)=>{
-//         console.log(`worker ${worker.process.pid} died`)
-//         cluster.fork()
-//     })
-// }
-//     else {
-//         app.listen(5000, ()=> console.log(`server is running 5000 process id is ${process.pid}`))
-//     }
+    for(i=0;i< numCpu;i++) {
+        cluster.fork() // uses the child process.
+    }
+    cluster.on('exit', (worker, code, signal)=>{
+        console.log(`worker ${worker.process.pid} died`)
+        cluster.fork() //once worker died we are creating new worker.
+    })
+}
+    else {
+        app.listen(5000, ()=> console.log(`server is running 5000 process id is ${process.pid}`))
+    }
 
-    app.listen(5000, ()=> console.log(`server is running 5000 process id is ${process.pid}`))
+    // app.listen(5000, ()=> console.log(`server is running 5000 process id is ${process.pid}`))
 
 
 
